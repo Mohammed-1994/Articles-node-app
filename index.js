9ce8cb07fb9dd7f73a8235589b299f7cc27f693c
Mohammed Awad <merooa937@gmail.com>
2025-05-14 13:51:22 +0300

    # Add Environment Variables for connectd with mongodb and port. #  Add entry validator ( Joi )  when add an article to calidat title and content. # Add README file. # Unified responses over all applicatoin.

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
+require("dotenv").config();
+
 // impotrt express library
 const express = require("express");
 const app = express();
 
 const artcileRouter = require("./routers/articleRouters");
-
-app.use("/", artcileRouter)
+app.use("/", artcileRouter);
 
 const mongoose = require("mongoose");
 
-//mongog db connect link
-//  mongodb+srv://merooa937:<db_password>@cluster0.6kkuex2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-const mongooseUrl =
-  "mongodb+srv://merooa937:amazoona-14@cluster0.6kkuex2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const mongooseUrl = process.env.MONGODB_URL;
+
+const port = process.env.PORT;
+
 
 /**
  * connect the server with the database.
- *
  */
 mongoose
   .connect(mongooseUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 30000, // زيادة المهلة إلى 30 ثانية
+    serverSelectionTimeoutMS: 30000,
   })
   .then(() => {
     console.log("DB connected successfully.");
@@ -30,6 +28,6 @@ mongoose
     console.log("error with connect to DB ", error);
   });
 
-app.listen(3000, () => {
-  console.log("I'm lestening in port 3000");
+app.listen(port, () => {
+  console.log(`I'm lestening in port: ${port}`);
 });
